a3ecc2203bdfa5d409c08f7a7c24095705ca751e
tkachuks <serhiytkachuk88@gmail.com>
2019-06-20 10:16:23 +0300

    upd opened preview for pricing page

--- src/components/pricing/component/openedPreview/index.jsx
+++ src/components/pricing/component/openedPreview/index.jsx
@@ -1,37 +1,45 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import style from './style.less';
 import './style.less';
 
-export default ({name, icon, value}) => {
-	const marker = {'listStyleImage': 'url(' + config.urls.static + 'ic_check_mark.svg' + ')'}
-	return (
-		<div class={style.detail_price}>
-			<div class={style.header}>
-				<div class={style.icon_wrap}>
-					<img src={config.urls.static + icon} />
+export default class OpenedPreview extends Component {
+	state = {
+		switchBox: true
+	}
+	changeSwitch = () => this.setState({switchBox: !this.state.switchBox})
+	render() {
+		const marker = {'listStyleImage': 'url(' + config.urls.static + 'ic_check_mark.svg' + ')'};
+		const { name, icon, value } = this.props;
+		const { switchBox } = this.state;
+		return (
+			<div class={style.detail_price}>
+				<div class={style.header}>
+					<div class={style.icon_wrap}>
+						<img src={config.urls.static + icon} />
+					</div>
+					<div class={style.header_text}>
+						<p class={style.name}>{config.translations.pricing.data[name].opened_preview.name}</p>
+						<p class={style.type}>{config.translations.pricing.data[name].opened_preview.business_type}</p>
+						<p class={style.cost}><span class={style.value}>{config.translations.pricing.data[name].opened_preview.group_preview_price.replace('{currency}', config.modules.pricing.currency)
+							.replace('{price_value}', value)}</span>{config.translations.pricing.data[name].opened_preview.period}</p>
+					</div>
 				</div>
-				<div>
-					<p>{config.translations.pricing.data[name].opened_preview.name}</p>
-					<p>{config.translations.pricing.data[name].opened_preview.business_type}</p>
-					<p><span>{config.translations.pricing.data[name].opened_preview.group_preview_price.replace('{currency}', config.modules.pricing.currency)
-						.replace('{price_value}', value)}</span>{config.translations.pricing.data[name].opened_preview.period}</p>
-				</div>
+				{config.translations.pricing.data[name].opened_preview.price_monthly &&
+				config.translations.pricing.data[name].opened_preview.price_yearly && <div class={style.switch_section}>
+					<p class={switchBox ? style.active : style.inactive}>{config.translations.pricing.data[name].opened_preview.price_yearly}</p>
+					<label class={style.switch} >
+						<input value={switchBox} type='checkbox' onChange={this.changeSwitch} />
+						<span class={style.slider}></span>
+					</label>
+					<p class={switchBox ? style.inactive : style.active}>{config.translations.pricing.data[name].opened_preview.price_monthly}</p>
+				</div>}
+				<ul>
+					{config.translations.pricing.data[name].opened_preview.features.map(item => (
+						<li class={style.feature} style={marker}>{item}</li>
+					))}
+				</ul>
+				<a class={style.link} href={config.urls.signup}><button class={style.paid}>{config.translations.pricing.data[name].opened_preview.cta_label}</button></a>
 			</div>
-			{config.translations.pricing.data[name].opened_preview.price_monthly &&
-			config.translations.pricing.data[name].opened_preview.price_yearly && <div class={style.switch_section}>
-				<p>{config.translations.pricing.data[name].opened_preview.price_yearly}</p>
-				<label class={style.switch}>
-					<input type='checkbox' />
-					<span class={style.slider}></span>
-				</label>
-				<p>{config.translations.pricing.data[name].opened_preview.price_monthly}</p>
-			</div>}
-			<ul>
-				{config.translations.pricing.data[name].opened_preview.features.map(item => (
-					<li style={marker}>{item}</li>
-				))}
-			</ul>
-			<button>{config.translations.pricing.data[name].opened_preview.cta_label}</button>
-		</div>
-	);
-};
+		);
+	}
+}
