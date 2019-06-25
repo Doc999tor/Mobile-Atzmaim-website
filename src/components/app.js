988486e60dad5b048822fc82c58a0648c00a95f2
tkachuks <serhiytkachuk88@gmail.com>
2019-06-25 10:31:32 +0300

    upd active link in nav

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
 import { h, Component } from 'preact';
-import { Router } from 'preact-router';
+import { Router, route } from 'preact-router';
 
 import Header from './header';
 import Home from './home';
@@ -14,14 +14,20 @@ export default class App extends Component {
 	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
 	 *	@param {string} event.url	The newly routed URL
 	 */
+	state = {
+		match: ''
+	}
 	componentDidMount = () => {
-    if (config.isRTL) document.getElementsByTagName('body')[0].style.direction = 'rtl';
-  }
+		if (config.isRTL) document.getElementsByTagName('body')[0].style.direction = 'rtl';
+	}
 	handleRoute = e => {
+		console.log('e.url', e.url);
+		this.setState({match: e.url});
 		this.currentUrl = e.url;
 	};
 
 	render() {
+		const { match } = this.state;
 		return (
 			<div id="app">
 				<Header />
@@ -32,7 +38,7 @@ export default class App extends Component {
 					<Pricing path="/pricing" />
 					<Reviews path="/reviews" />
 				</Router>
-				<Navigation />
+				<Navigation match={match} />
 			</div>
 		);
 	}
--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -2,10 +2,12 @@ import { h } from 'preact';
 import { Link } from 'preact-router';
 import style from './style.less';
 
-export default () => (
-	<nav class={style.navigation}>
-		{config.navigation.map(link => (
-			<Link class={style.link} href={link.link}><img src={config.urls.static + link.icon} alt={link.name}/>{link.name}</Link>
-		))}
-	</nav>
-);
+export default ({ match }) => {
+	return (
+		<nav class={style.navigation}>
+			{config.navigation.map(link => (
+				<Link activeClassName="active" class={match === link.link ? style.match : style.link} href={link.link}><img src={config.urls.static + (match === link.link ? link.icon : link.icon)} alt={link.name}/>{link.name}</Link>
+			))}
+		</nav>
+	);
+};
