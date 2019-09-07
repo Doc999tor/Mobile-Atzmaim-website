0e75d46f0fd401d753520227badff2265b800f49
tkachuks <serhiytkachuk88@gmail.com>
2019-09-07 17:53:07 +0300

    fix styles

--- src/components/business_types/component/index.jsx
+++ src/components/business_types/component/index.jsx
@@ -11,13 +11,13 @@ export default class Bussiness extends Component {
 		const { name, icon } = this.props
 		const { animation } = this.state
 		return (
-			<div class={this.state.animation ? style.main2 : style.main} onClick={this.init}>
+			<div class={`${style.common} ${this.state.animation ? style.main2 : style.main}`} onClick={this.init}>
 				<div class={this.state.animation ? style.sub2 : style.sub}>
 					<img src={config.urls.media + icon + '.jpg'} />
 				</div>
 				<div class={this.state.animation ? style.bottom_info2 : style.bottom_info}>
 					<p class={style.title}>{config.translations.business_types.content[name].title}</p>
-					{animation &&	<div class={style.extend1}>
+					{animation &&	<div>
 						<div class={style.line} />
 						<p class={style.text}>{config.translations.business_types.content[name].text}</p>
 					</div>}
