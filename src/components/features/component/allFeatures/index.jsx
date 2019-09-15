6f51d7148a1d4d815c4fcf4cfc425a6edb75df77
tkachuks <serhiytkachuk88@gmail.com>
2019-09-15 03:14:27 +0300

    upd & join all animations in future page

--- src/components/features/component/allFeatures/index.jsx
+++ src/components/features/component/allFeatures/index.jsx
@@ -5,10 +5,10 @@ import style from './all.less'
 export default ({ selectFeature, animations, secondAnimation, activeLink }) => {
 	const firstP = animations ? `${style.phone_act} ${style.start}` : style.phone_act
 	const firstW = animations ? `${style.active_woman_l} ${style.start}` : style.active_woman_l
-	// const second = animations ? `${style.outer_ltr} ${style.end}` : style.outer_ltr
-	const firstPhone = !secondAnimation ? `${style.active_phone_r} ${style.start_p_r}` : style.active_phone_r
+	const secondP = animations ? `${style.phone_act_r} ${style.end}` : style.phone_act_r
+	const secondW = animations ? `${style.active_woman_r} ${style.end}` : style.active_woman_r
 	return (
-		<div class={style.container}>
+		<div class={`${style.container} ${!secondAnimation && style.bgr}`}>
 			<section class={style.top_section}>
 				<div class={`${style.text} ${secondAnimation && style.text_end}`}>
 					{secondAnimation && <h2>{config.translations.features.content.title}</h2>}
@@ -17,17 +17,17 @@ export default ({ selectFeature, animations, secondAnimation, activeLink }) => {
 					{!secondAnimation
 						? <img class={config.isRTL ? `${style.scale} ${style.start_bg}` : style.start_bg} src={config.urls.media + 'bg_top.svg'} />
 						: <img class={config.isRTL ? `${secondAnimation && style.inner_rtl}` : `${secondAnimation && style.inner_ltr}`} src={config.urls.media + 'bg_top.svg'} />}
-					{secondAnimation
-						 ? <img class={config.isRTL ? `${style.phone_begin} ${style.scale}` : style.phone_begin} src={config.urls.media + 'active_phone.svg'} />
-						 : <img class={config.isRTL ? firstPhone : `${secondAnimation && firstP}`} src={config.urls.media + 'active_phone.svg'} />}
+					{!secondAnimation
+						 ? <img class={config.isRTL ? `${style.phone_begin_r} ${style.scale}` : style.phone_begin} src={config.urls.media + 'active_phone.svg'} />
+						 : <img class={config.isRTL ? `${secondAnimation && secondP}` : `${secondAnimation && firstP}`} src={config.urls.media + 'active_phone.svg'} />}
 					{!secondAnimation && <Fragment>
-						<img class={config.isRTL ? style.blank_phone_r : style.blank_phone_l} src={config.urls.media + 'phone_fch.svg'} />
-						<img class={config.isRTL ? style.blank_woman_r : style.blank_woman_l} src={config.urls.media + 'woman_fch.svg'} />
+						<img class={config.isRTL ? `${style.scale} ${style.blank_phone_r}` : style.blank_phone_l} src={config.urls.media + 'phone_fch.svg'} />
+						<img class={config.isRTL ? `${style.scale} ${style.blank_woman_r}` : style.blank_woman_l} src={config.urls.media + 'woman_fch.svg'} />
 					</Fragment>
 					}
 					{!secondAnimation
-						? <img class={config.isRTL ? firstPhone : style.wmn} src={config.urls.media + 'active_woman.svg'} />
-						: <img class={config.isRTL ? firstPhone : `${secondAnimation && firstW}`} src={config.urls.media + 'active_woman.svg'} />}
+						? <img class={config.isRTL ? style.wmn_r : style.wmn} src={config.urls.media + 'active_woman.svg'} />
+						: <img class={config.isRTL ? `${secondAnimation && secondW}` : `${secondAnimation && firstW}`} src={config.urls.media + 'active_woman.svg'} />}
 				</div>
 			</section>
 			<div class={`${style.wrap} ${secondAnimation && style.wrap_fix}` }>
