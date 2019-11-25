c5f8400e91a849c05497efb33f2d76e84f4409fa
tkachuks <serhiytkachuk88@gmail.com>
2019-11-25 13:49:24 +0200

    animated price page

--- src/components/pricing/component/commonInfo/index.jsx
+++ src/components/pricing/component/commonInfo/index.jsx
@@ -1,4 +1,4 @@
-import { h, Component } from 'preact'
+import { h, Fragment, Component } from 'preact'
 import SmallPreview from '../smallPreview'
 import DetailInfo from '../detailInfo'
 import styles from '../../../features/component/allFeatures/all.less'
@@ -21,10 +21,28 @@ export default class CommonInfo extends Component {
 					<div class={`${styles.text} ${animation && styles.text_end}`}>
 						{animation && <h2>{config.translations.pricing.title}</h2>}
 					</div>
-
 					<div class={styles.background_top}>
-						<img class={config.isRTL ? styles.inner_rtl : styles.inner_ltr} src={config.urls.media + 'bg_top.svg'} />
-						<img class={config.isRTL ? style.outer_rtl : style.outer_ltr} src={config.urls.media + 'ill_pricing.svg'} />
+						{!animation
+							? <Fragment>
+								<img class={config.isRTL ? `${styles.scale} ${styles.start_bg}` : styles.start_bg} src={config.urls.media + 'bg_top.svg'} />
+								<img class={config.isRTL ? style.outer_rtl : style.outer_ltr} src={config.urls.media + 'man_active.svg'} />
+							</Fragment>
+							: <Fragment>
+								<img class={config.isRTL ? styles.inner_rtl : styles.inner_ltr} src={config.urls.media + 'bg_top.svg'} />
+								<img class={config.isRTL ? style.outer_rtl : style.outer_ltr} src={config.urls.media + 'screen.svg'} />
+							</Fragment>
+						}
+						{
+							!animation && <img class={config.isRTL ? style.man_rtl : style.man_ltr} src={config.urls.media + 'man.svg'} />
+						}
+						{!animation
+							? <img class={config.isRTL ? style.man_start_rtl : style.man_start_ltr} src={config.urls.media + 'man.svg'} />
+							: <img class={config.isRTL ? `${animation && style.man_end_rtl}` : `${animation && style.man_end_ltr}`} src={config.urls.media + 'man_active.svg'} />
+						}
+						{!animation
+							? <img class={config.isRTL ? style.coin_start_rtl : style.coin_start_ltr} src={config.urls.media + 'coin.svg'} />
+							: <img class={config.isRTL ? style.coin_rtl : style.coin_ltr} src={config.urls.media + 'coin.svg'} />
+						}
 					</div>
 				</section>
 				<section class={`${typesStyl.wrap} ${animation && typesStyl.wrap_end}`}>
