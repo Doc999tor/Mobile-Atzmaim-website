9501c36cf1552a9552ffdf1b1a0349715fadac7c
tkachuks <serhiytkachuk88@gmail.com>
2019-12-09 23:39:01 +0200

    updated animation on hero page(with @keyframes)

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -11,10 +11,10 @@ export default class Hero extends Component {
 		const background = { backgroundImage: 'url(' + config.urls.media + 'pic_bg.jpg' + ')' }
 		const features = config.modules.features.data.filter(i => config.modules.hero.features.includes(i.name))
 		return (
-			<div id='hero' class={`${style.hero} ${style.height}`} >
-				<div class={style.full} style={animation ? background : ''}>
-					<div class={`${style.common} ${animation ? style.shadow : style.bgr}`} >
-						<div class={`${style.content} ${animation && style.padding_end}`}>
+			<div id='hero' class={style.height} >
+				<div class={`${style.full} ${animation && style.backgroundImg}`} style={animation ? background : ''}>
+					<div class={`${style.common} ${animation && (style.shadow + ' ' + style.test)}`} >
+						<div class={`${style.content} ${animation && (config.isRTL ? style.padding_animation_rtl : style.padding_animation_ltr)}`}>
 							{animation && <h2>{config.translations.hero_page.title}</h2>}
 							{animation && <div class={style.feature_wrap}>
 								{features.map(f => (
@@ -35,7 +35,7 @@ export default class Hero extends Component {
 						<div class={style.phone_wrap}>
 							{!animation
 								? <img class={style.black_phone} src={config.urls.media + 'black_phone.png'} height='366' width='183' loading='lazy' alt='phone animation' />
-								: <img class={`${style.start_p} ${animation && style.end_p}`} src={config.urls.media + 'pic_iphone.png'} height='366' width='183' loading='lazy' alt='phone animation' />}
+								: <img class={animation && style.phone_animation} src={config.urls.media + 'pic_iphone.png'} height='366' width='183' loading='lazy' alt='phone animation' />}
 						</div>
 					</div>
 				</div>
