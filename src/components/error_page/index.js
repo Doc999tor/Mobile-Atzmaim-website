803a42e5f6ff8efe46de2b6168b56defe447bb56
tkachuks <serhiytkachuk88@gmail.com>
2019-12-17 12:43:19 +0200

    max width for desktop

--- src/components/error_page/index.js
+++ src/components/error_page/index.js
@@ -7,21 +7,23 @@ export default ({ referer }) => {
 	return (
 		<div id='error' class={style.error} >
 			<Header referer={referer} />
-			<div class={style.top_section}>
-				<div class={style.title}>
-					<h2 >{config.translations.error_page.title}</h2>
-					<p>{config.translations.error_page.text}</p>
+			<div class={style.error_wrap}>
+				<div class={style.top_section}>
+					<div class={style.title}>
+						<h2 >{config.translations.error_page.title}</h2>
+						<p>{config.translations.error_page.text}</p>
+					</div>
+					<div class={style.background_top}>
+						<img class={config.isRTL ? style.inner_rtl : style.inner_ltr} src={config.urls.media + 'bg_top.svg'} alt='background' />
+						<img class={config.isRTL ? style.outer_rtl : style.outer_ltr} src={config.urls.media + 'ill_robot.svg'} alt='robot' />
+						<img class={config.isRTL ? style.question_rtl : style.question_ltr} src={config.urls.media + 'question.svg'} alt='question image' />
+					</div>
 				</div>
-				<div class={style.background_top}>
-					<img class={config.isRTL ? style.inner_rtl : style.inner_ltr} src={config.urls.media + 'bg_top.svg'} alt='background' />
-					<img class={config.isRTL ? style.outer_rtl : style.outer_ltr} src={config.urls.media + 'ill_robot.svg'} alt='robot' />
-					<img class={config.isRTL ? style.question_rtl : style.question_ltr} src={config.urls.media + 'question.svg'} alt='question image' />
+				<div class={style.background_bottom}>
+					<Link class={`${style.button} ${style.home}`} href={config.baseUrl}><span><img src={config.urls.media + 'ic_home.svg'} alt={config.translations.error_page.home_btn} /></span>{config.translations.error_page.home_btn}</Link>
+					<a class={style.button} href={config.urls.support}><span><img src={config.urls.media + 'ic_support.svg'} alt={config.translations.error_page.support_btn} /></span>{config.translations.error_page.support_btn}</a>
+					<a class={style.button} href={config.urls.signup}><span><img src={config.urls.media + 'ic_join.svg'} alt={config.translations.error_page.join_btn} /></span>{config.translations.error_page.join_btn}</a>
 				</div>
-			</div>
-			<div class={style.background_bottom}>
-				<Link class={`${style.button} ${style.home}`} href={config.baseUrl}><span><img src={config.urls.media + 'ic_home.svg'} alt={config.translations.error_page.home_btn} /></span>{config.translations.error_page.home_btn}</Link>
-				<a class={style.button} href={config.urls.support}><span><img src={config.urls.media + 'ic_support.svg'} alt={config.translations.error_page.support_btn} /></span>{config.translations.error_page.support_btn}</a>
-				<a class={style.button} href={config.urls.signup}><span><img src={config.urls.media + 'ic_join.svg'} alt={config.translations.error_page.join_btn} /></span>{config.translations.error_page.join_btn}</a>
 			</div>
 		</div>
 	)
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -22,22 +22,24 @@ export default class Header extends Component {
 	render () {
 		const { active } = this.state
 		return (
-			<header class={style.header}>
-				<div class={style.cont}>
-					<button class={style.menu_btn} onClick={active ? this.closeMenu : this.menuOnOff}><img src={config.urls.media + (!active ? 'ic_menu.svg' : 'ic_menu_cross.svg')} alt='menu' /></button>
-					<a class={style.login_link} href={config.urls.login}>
-						<img src={config.urls.media + 'ic_lock.svg'} alt='lock' />
-						<span>{config.translations.hero.log_in}</span>
-					</a>
-				</div>
-				{this.props.referer !== 'application'
-					? active && <Menu close={this.closeMenu} />
-					: active && <MenuApp closeMenu={this.closeMenu} />}
-				<div class={style.cont}>
-					<img src={config.urls.media_logo + 'ic_logo.svg'} alt='Logo' />
-					<span class={style.logo}>{config.translations.hero.main_logo}</span>
-				</div>
-			</header>
+			<div class={style.header_wrap}>
+				<header class={style.header}>
+					<div class={style.cont}>
+						<button class={style.menu_btn} onClick={active ? this.closeMenu : this.menuOnOff}><img src={config.urls.media + (!active ? 'ic_menu.svg' : 'ic_menu_cross.svg')} alt='menu' /></button>
+						<a class={style.login_link} href={config.urls.login}>
+							<img src={config.urls.media + 'ic_lock.svg'} alt='lock' />
+							<span>{config.translations.hero.log_in}</span>
+						</a>
+					</div>
+					{this.props.referer !== 'application'
+						? active && <Menu close={this.closeMenu} />
+						: active && <MenuApp closeMenu={this.closeMenu} />}
+					<div class={style.cont}>
+						<img src={config.urls.media_logo + 'ic_logo.svg'} alt='Logo' />
+						<span class={style.logo}>{config.translations.hero.main_logo}</span>
+					</div>
+				</header>
+			</div>
 		)
 	}
 }
