8ae848d570bd80e0e33a165b8632adb05722fe76
tkachuks <serhiytkachuk88@gmail.com>
2019-12-24 14:56:00 +0200

    added source for gallery

--- src/components/carousel/slide.jsx
+++ src/components/carousel/slide.jsx
@@ -4,7 +4,10 @@ import style from './carousel.less'
 export default ({ name, current }) => {
 	return (
 		<div class={`${style.slide} ${current ? style.active : ''}`}>
-			<img src={`${config.urls.hero_gallery}${name}`} alt={name} loading='lazy' />
+			<picture>
+				<source srcset={`${config.urls.hero_gallery}${name}.webp`} type='image/webp' loading='lazy' />
+				<img src={`${config.urls.hero_gallery}${name}.png`} alt={name} height='366' width='183' loading='lazy' />
+			</picture>
 		</div>
 	)
 }
