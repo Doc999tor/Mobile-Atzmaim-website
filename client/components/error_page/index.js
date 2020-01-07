56d30f9987bba7ee491c1d8b6efc28d869fdd77b
tkachuks <serhiytkachuk88@gmail.com>
2020-01-07 11:04:11 +0200

    added animation on error page

--- client/components/error_page/index.js
+++ client/components/error_page/index.js
@@ -7,14 +7,15 @@ export default ({ referer }) => {
 		<div id='error' class='error' >
 			<div class='error_wrap'>
 				<div class='error_top_section'>
-					<div class='error_title'>
+					<div class='error_title error_height'>
 						<h2 >{config.translations.error_page.title}</h2>
 						<p>{config.translations.error_page.text}</p>
 					</div>
 					<div class='error_background_top'>
-					<img class={config.isRTL ? `scale static_bg_rtl` : 'static_bg'} src={config.urls.media + 'bg_top.svg'} alt='background' />
+						<img class={config.isRTL ? `scale static_bg_rtl` : 'static_bg'} src={config.urls.media + 'bg_top.svg'} alt='background' />
 						<img class={config.isRTL ? 'error_outer_rtl' : 'error_outer_ltr'} src={config.urls.media + 'ill_robot.svg'} alt='robot' />
-						<img class={config.isRTL ? 'question_rtl' : 'question_ltr'} src={config.urls.media + 'ic_question.svg'} alt='question image' />
+						<img class={config.isRTL ? 'question_rtl' : 'question_ltr'} src={config.urls.media + 'question.svg'} alt='question image' />
+						<img class={config.isRTL ? 'robot_shadow_rtl self_shadow' : 'robot_shadow_ltr self_shadow'} src={config.urls.media + 'robot_shadow.svg'} alt='robot_shadow image' />
 					</div>
 				</div>
 				<div class='error_background_bottom'>
