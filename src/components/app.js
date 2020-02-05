d85f9e033c7144fc533c0b9b47f876974f8a86b6
tkachuks <serhiytkachuk88@gmail.com>
2020-02-05 13:27:33 +0200

    hidden asyncRouter

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
-import { Router, route } from 'preact-router'
+import { Router } from 'preact-router'
+// import { Router, route } from 'preact-router'
 import { disableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock'
-import AsyncRoute from 'preact-async-route'
+// import AsyncRoute from 'preact-async-route'
 import { h, Component } from 'preact'
 // import qs from 'qs'
 import Header from './header'
+import Main from './main'
+import ErrorPage from './error_page'
+import ContactUs from './contact_us'
 
 export default class App extends Component {
 	state = {
@@ -32,11 +36,11 @@ export default class App extends Component {
 		clearAllBodyScrollLocks()
 	}
 
-	getMain = (url, cb, props) => import('./main').then(module => module.default)
+	// getMain = (url, cb, props) => import('./main').then(module => module.default)
 
-	getError = (url, cb, props) => import('./error_page').then(module => module.default)
+	// getError = (url, cb, props) => import('./error_page').then(module => module.default)
 
-	getContactUs = (url, cb, props) => import('./contact_us').then(module => module.default)
+	// getContactUs = (url, cb, props) => import('./contact_us').then(module => module.default)
 
 	render () {
 		console.log('App', config)
@@ -44,7 +48,10 @@ export default class App extends Component {
 	    <div id='app'>
 				<Header active={this.state.active} menuOnOff={this.menuOnOff} closeMenu={this.closeMenu} referer={this.state.referer} />
 				<Router>
-					<AsyncRoute
+					<Main path={config.baseUrl + '/'} />
+					<ErrorPage path={config.baseUrl + '/error'} referer={this.state.referer} />
+					<ContactUs path={config.urls.contact_us} active={this.state.active} />
+					{/* <AsyncRoute
 						path={config.baseUrl + '/'}
 						getComponent={this.getMain}
 					/>
@@ -57,7 +64,8 @@ export default class App extends Component {
 						active={this.state.active}
 						path={config.urls.contact_us}
 						getComponent={this.getContactUs}
-					/>
+					/> */}
+
 				</Router>
 	    </div>
 	  )
