d409ab889d5bd6a849f69d77f36c3451aeed8223
tkachuks <serhiytkachuk88@gmail.com>
2020-05-24 09:14:19 +0300

    upd pricing page

--- src/components/pricing/component/detailInfo/index.jsx
+++ src/components/pricing/component/detailInfo/index.jsx
@@ -1,6 +1,5 @@
 import { h } from 'preact'
 import { useState } from 'preact/hooks'
-import { route } from 'preact-router'
 import SwichBox from '../switchBox'
 import OpenedPreview from '../openedPreview'
 import style from './detail.less'
@@ -10,14 +9,27 @@ export default () => {
 	const handleChange = () => setValue((currentValue) => !currentValue)
 	const handleChooseYearly = () => setValue(true)
 	const handleChooseMonthly = () => setValue(false)
-	const goToHome = () => route(config.baseUrl)
+	const goBack = () => window.history.back()
 
 	return (
 		<div class={style.detail_info}>
-			<div className={style.swichBox_wrap}>
-				<SwichBox value={value} handleChange={handleChange} handleChooseYearly={handleChooseYearly} handleChooseMonthly={handleChooseMonthly} />
+			<div class={style.header_wrap}>
+				<header class={style.header}>
+					<div class={style.img_wrap}>
+						<button class={style.back_btn} onClick={goBack}>
+							<img class={style.back_img} src={config.urls.media + 'ic_arrow.svg'} alt='ic_arrow.svg' />
+						</button>
+					</div>
+					<h2 class={style.title}>{config.translations.pricing.main_title}</h2>
+					<div class={style.menu_btn_wrap}></div>
+				</header>
 			</div>
-			{config.modules.pricing.data.map(item => <OpenedPreview goHome={goToHome} name={item.name} icon={item.icon} item={item} />)}
+			<div class={style.content}>
+				<div className={style.swichBox_wrap}>
+					<SwichBox value={value} handleChange={handleChange} handleChooseYearly={handleChooseYearly} handleChooseMonthly={handleChooseMonthly} />
+				</div>
+				{config.modules.pricing.data.map(item => <OpenedPreview goHome={goBack} value={value} name={item.name} icon={item.icon} item={item} />)}
+			</div>
 		</div>
 	)
 }
