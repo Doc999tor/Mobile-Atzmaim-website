a2d1f608b6c9800d28bdccad565c1e41318f2662
tkachuks <serhiytkachuk88@gmail.com>
2020-06-16 11:18:53 +0300

    fix content size on hero page

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -61,7 +61,7 @@ export default class Hero extends Component {
   								<div class={`${style.content} ${animation && (config.isRTL ? styles.text_end_rtl : styles.text_end_ltr)}`}>
   									<div class={style.texts}>
   										{animation && <div class={style.title_container}><h2 class={style.title}>{config.translations.hero.carousel_text[i].title}</h2></div>}
-  										{animation && <div class={style.feature_wrap}>
+  										{animation && <div>
   											{features && features.map(f => {
   												const svgObj = iconsData.find(i => f.name === i.name)
   												return (
