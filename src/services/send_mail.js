583bbd2b856a976bf782541dd4a65df33de008be
tkachuks <serhiytkachuk88@gmail.com>
2020-06-18 18:01:37 +0300

    hidden to attach file

--- src/services/send_mail.js
+++ src/services/send_mail.js
@@ -3,6 +3,9 @@ import { default as mainRequestService } from '../../components-lib/request.serv
 export const postService = (url, body) => {
 	const options = {
 		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		},
 		body
 	}
 	return mainRequestService(url, options)
