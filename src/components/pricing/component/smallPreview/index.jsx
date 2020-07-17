71eeec59f91754f3a73c10bfdf0c2cebc7ed74e6
tkachuks <serhiytkachuk88@gmail.com>
2020-07-17 18:17:36 +0300

    added indent between words in small price card

--- src/components/pricing/component/smallPreview/index.jsx
+++ src/components/pricing/component/smallPreview/index.jsx
@@ -11,7 +11,7 @@ export default ({ preferred, name, icon, value, switchValue, handleShowOpenPlan 
 				<p class={style.price_name}>{config.translations.pricing.data[name].small_preview.name}</p>
 				<p class={style.for_whom}>{config.translations.pricing.data[name].small_preview.business_type}</p>
 				<p class={style.price_plan}><span class={style.currency}>{config.translations.pricing.data[name].small_preview.group_preview_price.replace('{currency}', config.modules.pricing.currency)
-					.replace('{price_value}', value)}</span><span class={style.per}>{config.translations.pricing.data[name].small_preview.period}</span></p>
+					.replace('{price_value}', value)}</span> <span class={style.per}>{config.translations.pricing.data[name].small_preview.period}</span></p>
 			</div>
 		</div>
 	);
