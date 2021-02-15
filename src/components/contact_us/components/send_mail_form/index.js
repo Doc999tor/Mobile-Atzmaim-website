3fbe41d49761228e1cde3db9c3e747cca4dd13e3
tkachuks <serhiytkachuk88@gmail.com>
2021-02-15 22:53:27 +0200

    added new validation

--- src/components/contact_us/components/send_mail_form/index.js
+++ src/components/contact_us/components/send_mail_form/index.js
@@ -1,6 +1,5 @@
 import { h, Component, Fragment, createRef } from 'preact'
-import { postService } from '../../../../services/send_mail.js'
-import validatePhone from '../../../../../components-lib/validate-phone'
+import { postService, postValidateService } from '../../../../services/send_mail.js'
 import { getCurrentFormatTime } from '../../../../helpers/helpers'
 import SendModal from '../send_mail_modal'
 import style from './sendForm.less'
@@ -9,7 +8,8 @@ export default class SendMailForm extends Component {
 	state = {
 		contact: '',
 		mail: '',
-		valid: true,
+		statusOutsideValidation: false,
+		incorrectNumber: false,
 		send: false,
 		sending: false
 	}
@@ -22,28 +22,67 @@ export default class SendMailForm extends Component {
  	this.contactDetail.current.focus()
  }
 
+	handleBlurPhone = () => {
+		const value = this.state.contact
+		if (value) {
+			const url = config.urls.validate_api
+			const body = `phone=${value}`
+			this.setState({
+				statusOutsideValidation: true
+			})
+			return postValidateService(body, url)
+				.then(({ status }) => {
+					if (status === 200) {
+						this.setState({
+							incorrectNumber: false
+						})
+						return true
+					}
+					if (status === 422) {
+						this.setState({
+							incorrectNumber: true
+						})
+						return false
+					}
+				})
+				.catch(error => console.log({ error }))
+				.finally(() => {
+					this.setState({
+						statusOutsideValidation: false
+					})
+				})
+		}
+	}
+
 	handleSendMailForm = e => {
 		e.preventDefault()
-		const { contact, mail, valid } = this.state
-		if (contact && mail && valid) {
-			this.setState({ send: true, sending: true }, () => {
-				setTimeout(() => {
-					const body = `contact_detail=${contact.trim()}&message=${mail.trim()}&added=${getCurrentFormatTime()}`
-					postService(config.urls.send_mail, body).then(r => {
-						if (r.status === 201) {
-							this.setState({
-								sending: false
-							}, () => {
-								setTimeout(() => {
-									window.history.back()
-								}, 2000)
+		const { contact, mail, incorrectNumber } = this.state
+		if (mail && this.handleValidation()) {
+			this.handleBlurPhone().then(res => {
+				if (res) {
+					this.setState({ send: true, sending: true }, () => {
+						setTimeout(() => {
+							const body = `contact_detail=${contact.trim()}&message=${mail.trim()}&added=${getCurrentFormatTime()}`
+							postService(config.urls.send_mail, body).then(r => {
+								if (r.status === 201) {
+									this.setState({
+										sending: false
+									}, () => {
+										setTimeout(() => {
+											window.history.back()
+										}, 2000)
+									})
+								}
 							})
-						}
+						}, 2000)
 					})
-				}, 2000)
+				}
 			})
-		} else if (!valid || !contact) {
+		} else if (incorrectNumber || contact?.trim() === '') {
 			this.contactDetail.current.focus()
+			this.setState({
+				incorrectNumber: true
+			})
 		} else if (!mail) {
 			this.textArea.current.focus()
 		}
@@ -53,34 +92,36 @@ export default class SendMailForm extends Component {
 		const value = e.target.value
 		const name = e.target.name
 		this.setState({
-			[name]: value
+			[name]: value,
+			incorrectNumber: name === 'contact' ? false : this.state.incorrectNumber
 		})
 	}
 
 	handleCloseModal = () => this.setState({ send: false })
 
 	handleValidation = () => {
-		const regMail = /(\w+@[a-zA-Z_]+?\.[a-zA-Z]{1,6})/
-		if (validatePhone(this.state.contact.trim()) || regMail.test(this.state.contact.trim())) {
+		if (this.state.contactValue?.trim() !== '' && !this.state.statusOutsideValidation && !this.state.incorrectNumber) {
 			this.setState({
-				valid: true
+				incorrectNumber: false
 			})
+			return true
 		} else {
 			this.setState({
-				valid: false
+				incorrectNumber: true
 			})
+			return false
 		}
 	}
 
 	render () {
-		const { valid, send, sending } = this.state
+		const { incorrectNumber, send, sending } = this.state
 		return (
 			<Fragment>
 				{!send
 					? <form id='send_form' onSubmit={this.handleSendMailForm} class={style.container}>
 						<h2 class={style.title}>{config.translations.contact_us.mobile.send_form_title}</h2>
-						<p class={style.text_label + (!valid ? ` ${style.not_valid_label}` : '')}>{config.translations.contact_us.contact_input_label}</p>
-						<input ref={this.contactDetail} placeholder={config.translations.contact_us.placeholder_contact} name='contact' onBlur={this.handleValidation} onInput={this.handleCangeInput} class={`${style.contact_input} ${!this.state.valid ? style.not_valid : ''}`} type='text' />
+						<p class={style.text_label + (incorrectNumber ? ` ${style.not_valid_label}` : '')}>{config.translations.contact_us.contact_input_label}</p>
+						<input ref={this.contactDetail} placeholder={config.translations.contact_us.placeholder_contact} name='contact' onBlur={this.handleValidation} onInput={this.handleCangeInput} class={`${style.contact_input} ${this.state.incorrectNumber ? style.not_valid : ''}`} type='tel' />
 						<p class={style.text_label}>{config.translations.contact_us.message_input_label}</p>
 						<textarea ref={this.textArea} placeholder={config.translations.contact_us.placeholder_message} name='mail' onInput={this.handleCangeInput} class={style.textarea} />
 						<div class={style.btn_wrap}>
